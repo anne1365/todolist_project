dc906b5af9f4c20c11a270aada6f0c7031bf1824
Anne Jones <anne1365@outlook.com>
2020-12-03 13:55:25 -0800

    todolist project additions: gemfile & stamp

---
+++ Gemfile.rb
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+ruby '2.5.0'
+
+gem 'minitest', '~> 5.13'
+gem 'minitest-reporters', '~> 1.4'
+gem 'simplecov' '~> 0.20'
--- test/todolist_test.rb
+++ test/todolist_test.rb
@@ -3,6 +3,7 @@ system 'cls'
 require 'simplecov'
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'date'
 Minitest::Reporters.use!
 SimpleCov.start
 
@@ -19,6 +20,16 @@ class TodoTest < Minitest::Test
     @list.add(@todo1)
     @list.add(@todo2)
     @list.add(@todo3)
+  end
+
+  def test_no_due_date
+    assert_nil(@todo1.due_date)
+  end
+
+  def test_no_due_date
+    due_date = Date.today + 3
+    @todo2.due_date = due_date
+    assert_equal(due_date, @todo2.due_date)
   end
 
   def test_to_a
